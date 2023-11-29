ca1418968de9a1ac11071a57bb33a30d4a99dc2d
Jean-YS <114813930+Jean-YS@users.noreply.github.com>
2023-11-29 15:05:19 -0500

    Delete Proyecto_DBD directory

--- Proyecto_DBD/planillaback/src/main/java/uni/edu/pe/planillaback/dao/PlanillaDao.java
+++
@@ -1,7 +0,0 @@
-package uni.edu.pe.planillaback.dao;
-
-import uni.edu.pe.planillaback.dto.Empleado;
-
-public interface PlanillaDao {
-    Empleado registrarEmpleado(Empleado empleado);
-}
--- Proyecto_DBD/planillaback/src/main/java/uni/edu/pe/planillaback/service/PlanillaService.java
+++
@@ -1,7 +0,0 @@
-package uni.edu.pe.planillaback.service;
-
-import uni.edu.pe.planillaback.dto.Empleado;
-
-public interface PlanillaService {
-    Empleado registrarEmpleado(Empleado empleado);
-}
